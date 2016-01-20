8218b39221b98d1ecff71d30f8bb5e585069f74d
Michael Sullivan <sapientia.veritas@gmail.com>
2016-01-20 13:37:59 -0800

    commit?

--- scripts/repo.js
+++ scripts/repo.js
@@ -18,14 +18,15 @@
   // };
 
   repos.requestRepos = function(callback) {
-    $.ajax({
+    $.get({
         url: '/github/users/scotist/repos' +
         '?per_page=100' + '&sort=updated',
-        type: 'GET',
-        success: function(data, message, xhr) {
-          repos.all = data;
-        }
-    }).done(callback);
+        // type: 'GET',
+    })
+    .done(function(data, message, xhr) {
+      repos.all = data;
+    })
+    .done(callback);
   };
 
   repos.with = function(attr) {
